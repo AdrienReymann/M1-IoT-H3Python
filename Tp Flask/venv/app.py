5debce5a73f00bb0130751036b77e39d7037ed02
Adrien R <adrien.reymann@outlook.coml>
2020-11-30 11:49:30 +0100

    Tp 1.2

--- Tp Flask/venv/app.py
+++ Tp Flask/venv/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 
 app=Flask(__name__)
 
@@ -11,13 +11,19 @@ def home():
 def hello(name=None):
    return render_template('hello.html',name=name)
 
-@app.route('/api/book')
-def book()
-    return render_template('book.html')
 
-@app.route('/api/book/<id>')
-def bookId(id=None)
-    return render_template('bookId.html',id=id)
+
+@app.route('/<bookId>')
+def idBook(id ):
+    books = json.load(open('books.json'))
+    return render_template('book.html', contenu=books[int(id)])
+
+@app.route('/titre/<bookName>')
+def titleBook(titre ):
+    books = json.load(open('books.json'))
+    for i in range(len(books)):
+        if books[i]['boobkName'] == bookName:
+            return render_template('book.html', contenu = books[i])
 
 if __name__=="__main__":
     app.run()
