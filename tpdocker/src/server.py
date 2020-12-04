2fb222fde6661b137d4e643fc6331a84929e0bb0
Adrien R <adrien.reymann@outlook.coml>
2020-12-04 16:13:54 +0100

    Tp_api

--- tpdocker/src/server.py
+++ tpdocker/src/server.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 server = Flask(__name__)
 
 @server.route("/")
@@ -6,4 +8,5 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-   server.run(host='0.0.0.0') 
+    port = int(os.environ.get('PORT', 5000))
+    server.run(host='0.0.0.0', port=port)
