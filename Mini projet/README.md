455c78fae1eb8336fa9470c9168db5e0329b16a6
Adrien R <adrien.reymann@outlook.coml>
2020-12-05 22:41:50 +0100

    Portfolio

---
+++ Mini projet/README.md
@@ -0,0 +1,19 @@
+Présentation du projet : 
+
+Portefolio : Site web contenant un résumé de mon profil professionnel.
+
+Architecture : 2 pages et 1 liste déroulante en barre de navigation 
+Page 1 : Présentation personnelle, contact et information sur moi 
+Page 2 : Page CV Résumé de mon CV ainsi qu'un accès en PDF a mon CV en bas de page
+Liste déroulante : Accès a mes différent projets fini ou en cours.
+
+Lancement : Déploiement sous Heroku 
+
+Ajout à faire dans le futur : 
+Page de contact avec un envoi de mail automatique avec la saisie de l'utilisateur et refonte du style 
+
+Lien Heroku : 
+
+https://shielded-sea-84207.herokuapp.com/cv 
+
+Je te conseil de le visauliser sur tel car j'ai un énorme bug sur la version ordinateur je vais tenter de regler cela dans les jours a venir
--- Tp_Mini_Projet/venv/app.py
+++ Tp_Mini_Projet/venv/app.py
@@ -1,19 +1,33 @@
 from flask import Flask,render_template
+#Flask : micro-framework en open source permettant de faire de developpement web en python
 
 app=Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Home"
-
+#déclaration de l'objet
 
 @app.route("/")
+#objet.route -> permet de créer un lien afin de naviguer entre les différente page web
 def home():
-    return render_template("home.html")
-
-@app.route("/about")
-def about():
+    # retourne la page About
+    # render_template: permet d'afficher une page html et de lui retourné des information
     return render_template("About.html")
 
+@app.route("/cv")
+#objet.route -> permet de créer un lien afin de naviguer entre les différente page web
+def CV():
+    # retourne la page CV
+    #render_template: permet d'afficher une page html et de lui retourné des information
+    return render_template("Cv.html")
+
+@app.route("/contact")
+#objet.route -> permet de créer un lien afin de naviguer entre les différente page web
+def contact():
+    #retourne la page contact
+    return "Contact"
 if __name__ == "__main__" :
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    #La variable port specifie le port a utilisé lors du lancement de l'application, le port d'utilisation doit être utilisé pour le lancement de l'application sur Heroku
+    server.run(host='0.0.0.0', port=port)
+    #permet de lancer lapplication sur heroku le "host=0.0.0.0" permet à heroku de lui donnée une adresse.
+    #app.run(debug=True)
+    #permet de lancer l'application en local sur l'adresse 127.0.0.1.
+    #le paramètre debug permet de faire des modification sur les fichiers sans avoir à arrété et relancer le serveur a chaque modification celui-ci n'est pas obligatoire pour le deploiement sur heroku
